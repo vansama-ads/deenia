cb7fd4a416fa6122db795693ca1a8ea00f0e4672
vansama-ads <rukanarutin40@gmail.com>
2026-09-03 15:34:12 +0700

    fix B2 storage region

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,17 +60,16 @@ return [
             'report' => false,
         ],
         
-            'b2' => [
-        'driver' => 's3',
-        'key' => env('B2_KEY_ID'),
-        'secret' => env('B2_APPLICATION_KEY'),
-        'region' => env('B2_REGION'),
-        'bucket' => env('B2_BUCKET'),
-        'endpoint' => env('B2_ENDPOINT'),
-        'use_path_style_endpoint' => true,
-        'throw' => false,
-        'report' => false,
-    ],
+'b2' => [
+    'driver' => 's3',
+    'key' => env('AWS_ACCESS_KEY_ID'),
+    'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    'region' => env('AWS_DEFAULT_REGION'),
+    'bucket' => env('AWS_BUCKET'),
+    'endpoint' => env('AWS_ENDPOINT'),
+    'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+    'throw' => false,
+],
 
     ],
 
